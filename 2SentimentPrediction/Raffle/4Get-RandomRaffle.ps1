7ceeeee7dc14b922d96adf892e58c20f0c8eaf7b
hiramfleitas <hiramfleitas@hotmail.com>
2019-11-13 11:28:13 -0500

    fixed 4Get-RandomRaffle.ps1

--- 2SentimentPrediction/Raffle/4Get-RandomRaffle.ps1
+++ 2SentimentPrediction/Raffle/4Get-RandomRaffle.ps1
@@ -1,3 +1,12 @@
-$Evals = $(foreach ($line in Get-Content "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\2Eval.txt") {$line.tolower().split(" ")}) | sort | Get-Unique
-echo $Evals > "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\unique-sorted.txt"
-Get-Random -Input $Evals -Count 2 > "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\Winners.txt"
+$2Eval = "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\2Eval.txt"
+$Sorted = "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\unique-sorted.txt"
+$Winners = "C:\fleitasarts\seattle2019\2SentimentPrediction\Raffle\Winners.txt"
+
+[System.IO.File]::WriteAllText(
+    $2Eval,
+    ([System.IO.File]::ReadAllText($2Eval) -replace " ", "")
+)
+
+$Evals = $(foreach ($line in Get-Content $2Eval) {$line.tolower().split(" ")}) | sort | Get-Unique
+echo $Evals > $Sorted
+Get-Random -Input $Evals -Count 2 > $Winners
